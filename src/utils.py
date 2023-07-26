286c1cdf93167111920c873e400a7e5bb206a623
Olga Kahn <ozziekahn@gmail.com>
2023-07-26 01:29:53 -0400

    adding TSLA probability distribution notebook

--- src/utils.py
+++ src/utils.py
@@ -6,9 +6,21 @@ import nltk
 nltk.download('punkt')
 from textblob import TextBlob
 
-keywords = ['feel', 'believe', 'sense', 'why does', 'how does', 'how come', 'how much', 'when will', 
-            'isn’t it', 'wait for', 'add to', 'should I', 'should we','scale in', 'scale out'
+keywords_original = ["feel", "believe", "sense", "why does", "how does", "how come", "how much", 
+"when will", "isn’t it", "wait for", "add to", "should I", "should we", "scale in", "scale out"
            ]
+
+keywords = [
+    "to the moon", "diamond hands", "printing money", "rocketing", "skyrocketing", "moonshot", 
+    "going parabolic", "stonks only go up", "tendies", "loaded", "rolling in dough", "yolo'd", 
+    "hit the jackpot", "we're rich, baby", "feeling bullish", "bonkers", "confused", "don't understand", 
+    "what's happening", "can't believe", "mind blown", "can't wrap my head around", "baffled", "stumped", 
+    "flabbergasted", "scratching my head", "out of the loop", "lost", "in the dark", 
+    "can't make heads or tails", "doesn't compute", "doesn't add up", "what the heck", "bamboozled", 
+    "puzzled", "thrown for a loop", "at a loss", "what in the stonks", "surprised", "feel", "believe", 
+    "sense", "why does", "how does", "how come", "how much", "when will", "isn’t it", "wait for", 
+    "add to", "should I", "should we", "scale in", "scale out"
+]
 
 def get_posts_df(stock_name, stock_ticker):
     df_wsb = pd.read_csv('../data/raw/wallstreetbets/posts.csv')
@@ -30,7 +42,7 @@ def get_posts_df(stock_name, stock_ticker):
 
     posts['date'] = pd.to_datetime(df_noNan['created_utc'], unit='s')
 
-    posts_df = posts[['title', 'text', 'date', 'removed_by_category','link_flair_text', 'full_link', 'url', 'id', 'link_flair_text', 'over_18', 'pinned']]
+    posts_df = posts[['title', 'text', 'date', 'removed_by_category','link_flair_text', 'full_link', 'url', 'id', 'over_18', 'pinned']]
     print("Useful cols only: ", posts_df.shape)
     
     return posts_df
@@ -50,7 +62,47 @@ def get_stock_prices(ticker, start_date, end_date):
     return df
 
 
-def get_total_keyword_df(df, frequency_string): # input 'D' or 'H'
+def count_bewildered_keywords(df, keywords):
+    # Function to count number of bewildered keywords in a string
+    def count_keywords(text):
+        if isinstance(text, str):
+            text = text.lower()
+            count = sum(text.count(keyword) for keyword in keywords)
+            return count
+        else:
+            return 0
+        
+    # # Fill missing values in 'text' column with empty strings
+    # posts_df['text'].fillna("", inplace=True)
+
+    # Function to count total number of words in a string
+    def count_words(text):
+        if isinstance(text, str):
+            return len(text.split())
+        else:
+            return 0
+
+    # Apply the functions to 'title' and 'text' columns and store the results in new columns
+    df['keyword_count'] = df['title'].apply(count_keywords) + df['text'].apply(count_keywords)
+    df['word_count'] = df['title'].apply(count_words) + df['text'].apply(count_words)
+
+    # Calculate the percentage of keywords and store the result in a new column
+    df['keyword_percentage'] = ((df['keyword_count'] / df['word_count'] * 100)).round(2)
+    
+    # Define a new order of columns
+    new_order = ['keyword_count', 'word_count', 'keyword_percentage', 'title', 
+                 'text', 'date', 'id', 'link_flair_text', 'over_18', 
+                 'pinned', 'removed_by_category', 'full_link', 'url']
+    
+    # Reorder the DataFrame
+    df = df.reindex(columns=new_order)
+    
+    return df.sort_values(by='keyword_percentage', ascending=False)
+
+
+
+
+def get_total_keyword_df(df, keywords, frequency_string): # input 'D' or 'H'
     
     # Function to count keyword mentions
     def keyword_mentions(text):
@@ -73,6 +125,7 @@ def get_total_keyword_df(df, frequency_string): # input 'D' or 'H'
     # Create a new DataFrame with 'date' as index, 'title' and 'text' as columns
     new_df = df[['date', 'title', 'text']].copy()
     new_df.set_index('date', inplace=True)
+    # print(type(new_df.index))
     
     # Concatenate 'title' and 'text' columns and then resample
     concatenated_text = (new_df['title'].fillna('') + ' ' + new_df['text'].fillna(''))
@@ -96,7 +149,7 @@ def get_total_keyword_df(df, frequency_string): # input 'D' or 'H'
 
 
 
-def get_per_keyword_df(posts_df, frequency_string):
+def get_per_keyword_df(posts_df, keywords, frequency_string):
     # Function to tokenize text and count occurrences of keywords; returns percentages per keword
     def keyword_percentage_per_keyword(text_series):
         text_series = text_series.astype(str).replace('nan', '')
